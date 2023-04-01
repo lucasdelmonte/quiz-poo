59c4f6ee00c5c1bf24928ea8c635b17850df60f1
lucasdelmonte <delmontelucas678@gmail.com>
2023-04-01 12:48:17 -0300

    basic styles and data

---
+++ data/data.js
@@ -0,0 +1,33 @@
+const data = [
+    {
+        question: 'Which of the following options is not a code editor?',
+        choices: [
+            'vim',
+            'vscode',
+            'emacs',
+            'word'
+        ],
+        answer: 'word'
+    },
+    {
+        question: 'Which language is not object oriented?',
+        choices: [
+            'java',
+            'haskell',
+            'c++',
+            'phyton'
+        ],
+        answer: 'word'
+    },
+    {
+        question: 'What language is not used to style websites?',
+        choices: [
+            'stylus',
+            'less',
+            'sass',
+            'nextcss',
+            'postcss'
+        ],
+        answer: 'nextcss'
+    }
+]
